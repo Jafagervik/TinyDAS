73d18cdd7f428fbea7dc1a01f09c9b59871bc566
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2024-06-05 17:32:57 +0200

    changes

--- src/models/ae.py
+++ src/models/ae.py
@@ -1,16 +1,31 @@
 from tinygrad.nn import Tensor
+from tinygrad import nn
 from .base import BaseAE
 from ..loss import mse
 
 class AE(BaseAE):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__()
-        self.net = [
+        self.M = kwargs["M"]
+        self.N = kwargs["N"]
+        self.inp = self.M * self.N
+        self.hidden = kwargs["hidden"]
+        self.latent = kwargs["latent"]
 
+        self.net = [
+            nn.Linear(self.inp, 2048), nn.gelu, Tensor.dropout,
+            nn.Linear(2048, 1024), nn.gelu, Tensor.dropout,
+            nn.Linear(1024, 256), nn.gelu, Tensor.dropout,
+            nn.Linear(256, 128), nn.gelu, Tensor.dropout,
+
+            nn.Linear(128, 256), nn.gelu, Tensor.dropout,
+            nn.Linear(256, 1024), nn.gelu, Tensor.dropout,
+            nn.Linear(1024, 2048), nn.gelu, Tensor.dropout,
+            nn.Linear(2048, self.inp), nn.sigmoid
         ]
 
     def __call__(self, x: Tensor) -> Tensor:
-        return super().__call__(x)
+        return nn.sequential(self.net(x))
 
     def loss_function(self, model, f, X: Tensor):
         return super().loss_function(model, f, X)
