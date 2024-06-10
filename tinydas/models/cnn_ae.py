e205c6cb53f68ae9b2b1fefd492260df6c1d3c2a
Jafagervik <jorgenafagervik@gmail.com>
2024-06-10 05:07:01 +0200

    base for cnn ae is complete

--- tinydas/models/cnn_ae.py
+++ tinydas/models/cnn_ae.py
@@ -1,68 +1,32 @@
-from typing import Any
+from typing import Any, Optional
 
-from tinygrad import nn
+from tinygrad import dtypes, nn
 from tinygrad.nn import Tensor
 
 from tinydas.losses import mse
 from tinydas.models.base import BaseAE
 
-
-class Encoder:
-    def __init__(self) -> None:
-        self.c1 = self.conv_block(1, 32)
-        self.c2 = self.conv_block(32, 64)
-        self.c3 = self.conv_block(64, 128)
-        self.fc = nn.Linear(1000, 128)
-
-    def conv_block(self, inp: int, outp: int):
-        return nn.Conv2d(
-            in_channels=inp, out_channels=outp, kernel_size=(3, 3), stride=2, padding=1
-        )
-
-    def __call__(self, X: Tensor):
-        X = self.c1(X)
-        X = self.c2(X)
-        X = self.c3(X)
-        X = X.flatten()
-        return self.fc(X)
-
-
-class Decoder:
-    def __init__(self):
-        self.fc = nn.Linear(embedding_dim, np.prod(shape_before_flattening))
-        # store the shape before flattening
-        self.reshape_dim = shape_before_flattening
-        # define transpose convolutional layers
-        self.deconv1 = nn.ConvTranspose2d(
-            128, 128, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        self.deconv2 = nn.ConvTranspose2d(
-            128, 64, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        self.deconv3 = nn.ConvTranspose2d(
-            64, 32, kernel_size=3, stride=2, padding=1, output_padding=1
-        )
-        # define final convolutional layer to generate output image
-        self.conv1 = nn.Conv2d(32, channels, kernel_size=3, stride=1, padding=1)
-
-    def __call__(self, x: Tensor) -> Tensor:
-        x = self.fc(x)
-        x = x.view(x.size(0), *self.reshape_dim)
-        x = nn.relu(self.deconv1(x))
-        x = nn.relu(self.deconv2(x))
-        x = nn.relu(self.deconv3(x))
-        x = nn.sigmoid(self.conv1(x))
-        return x
+from ..losses import mae, mse
+from .base import BaseAE
 
 
 class CNNAE(BaseAE):
     def __init__(self, **kwargs):
         super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.M = kwargs["M"] or 625
+        self.N = kwargs["N"] or 2137
+        self.inp = self.M * self.N
+        self.hidden = 512  # kwargs["hidden"] or 128
+        self.latent = 256  # kwargs["latent"] or 64
+        self.f = mse if kwargs["loss"] == "mse" else mae
+
+        self.net = [
+            nn.Linear(self.hidden, self.inp),
+            Tensor.sigmoid,
+        ]
 
     def __call__(self, x: Tensor) -> Tensor:
-        return self.decoder(self.encoder(x))
+        return x.sequential(self.net)
 
     def criterion(self, X: Tensor) -> Tensor:
-        return mse(X, self(X))
+        return self.f(X, self(X))
